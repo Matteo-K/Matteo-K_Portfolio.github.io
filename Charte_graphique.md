ba0c9a098157db6d03d9bf0b50d94428e83943a1
Matteo-K <kervadecmatteo@gmail.com>
2024-05-15 09:12:32 +0200

    upload

--- Charte_graphique.md
+++ Charte_graphique.md
@@ -15,9 +15,31 @@ Cercle :
 
 Couleur :
 
-    élément important, header : #364968
     Corps : #FDDF97
     Encadrement, nav : #E09664
+
+font
+    clair
+    --clr-light-font--:#fff;
+    --clr-dark-font--:#000;
+    --clr-link-font--:#5cbfca;
+
+
+header
+    background
+    --clr-header-bg--:#364968;
+
+aside
+    background
+    --clr-aside-bg--:#555;
+
+    --clr-nav-bg--:brown;
+    --clr-main-bg--:brown;
+    --clr-section-bg--:brown;
+    --clr-search-bg1--:brown;
+    --clr-search-bg2--:brown;
+    --clr-search-icon--:brown;
+    --clr-footer-bg--:brown;
 
 ### Thème 2
 
@@ -38,3 +60,21 @@ Logo :
 
     width : 236.29px
     height : 102.02px       
+
+
+                        <p>
+                            Mattéo KERVADEC est un étudiant. Il étudie les différents systèmes, différents moyen d'information/communication. Mais surtout, il étudie la donnée.<br>
+                            Mattéo KERVADEC c'est moi, un étudiant dans le domaine de l'informatique.<br>
+                            Je commence à m'interesser à l'informatique en <span>4ème vers mes 13/14 ans</span>.<br>
+                            Dès cet âge là, je <span>commença à m'intérresser</i> aux différents langages informatique. Je finis par commencer doucement avec du <span>scratch</span>.<br>
+                            Puis, je <span>continue plus sérieusement</span> en 2nd vers 15/16 ans avec du <span>python et du html/css</span>. J'avait un <span>niveau très approximatif</span>. Mais cela me plaisait et je progressais assez rapidement.<br> 
+                            Et je comprennais les fondamentaux de la programmation</br>
+                            Très vite, l'année d'après, je dû prendre des spécialités pour continuer son parcours scolaire pour mon diplome de baccalauréat.<br>
+                            <span>Ne sachant pas</span> exactement ce que je voulait faire plus tard, je pris les spécialités <span>Mathématiques, Physique Chimie et Numérique Science et Informatique</span> (NSI).<br>
+                            Pour la terminale, je dus éliminer une de ses spécialités.<br>
+                            Je ne savais pas quoi choisir entre la physique et la NSI. Car au début, j'étais fasciné par la <span>curiositée</span> des sciences et le <span>mystère</span> de l'informatique.<br>
+                            Alors, il décida de garder les Mathématiques et la NSI car je vus un <span>meilleur potentiel</span> dans la programmation.<br>
+                            L'année de Terminale est achevé, je <span>reçu son diplome du BAC</span> avec la <span>mention assez bien</span> et je poursuivis mes études pour un <span>BUT informatique</span> dans le nord de la Bretagne à <span>Lannion</span>. <br>
+                            Arrivé en 1<sup>ère</sup>année du BUT, je dus me diversifier à travers plusieur langages de programmation.<br>
+                            Telle que le c, le java (pour redécouvrir la programmation objet), le php, le javascript et j'en passe.
+                        </p>
--- js/script.js
+++ js/script.js
@@ -1,109 +1,176 @@
-// Prends tout les éléments requis
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
-const suggBox = searchWrapper.querySelector("input + div"); // Liste des suggestions
-const icon = searchWrapper.querySelector("input+div+a"); // icon
-let lienTag = searchWrapper.querySelector(".search-input > a"); // Lien vers la page
-let page;
+/* ### Menu navigation ### */
+/* -- ouvre et ferme le menu à l'intéraction du bouton du menu -- */
 
-// Si l'utilisateur n'appuie pas sur la suggestions
-inputBox.onkeyup = (e) => {
-    let userData = e.target.value; //Valeur de l'entrée de l'utilisateur
-    let listeVide = [];
-    if (userData) {
-        document.addEventListener('keypress', function(e) {
-            if (e.key === 'Enter') {
-                page = `#`;
-                // Attribue à la page si le nom est exactement le même
-                for (let id = 0; id < suggestions.length; id++) {
-                    if (suggestions[id].projet_ === userData) {
-                        page = suggestions[id].lien;
-                    }
-                }
-                lienTag.setAttribute("href", page);
-                lienTag.click();
-            }
-        });
-        listeVide = suggestions.filter((data) => {
-            //filtre tout les suggestions contenant l'entrée de l'utilisateur
-            return data.projet_
-                .toLocaleLowerCase()
-                .includes(userData.toLocaleLowerCase());
-        });
-        listeVide = listeVide.map((data) => {
-            // Insert la suggestion
-            return (data = `<li>${data.projet_}</li>`);
-        });
-        searchWrapper.classList.add("active"); //afficher la boîte d'auto-completation
-        afficheSuggestions(listeVide);
-        let allList = suggBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //Ajoute sur un clic un attribue dans tout les <li>
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-    } else {
-        searchWrapper.classList.remove("active"); //afficher la boîte d'auto-completation
-    }
-};
+const menu_click = document.querySelector('header > i')
 
-function select(element) {
-    let lienPage;
-    let indice;
-    // parcours les suggetions et attribue le lien de la page
-    for (let id = 0; id < suggestions.length; id++) {
-        if (suggestions[id].projet_ === element.textContent) {
-            lienPage = suggestions[id].lien;
-            indice = id;
-        }
-    }
-    inputBox.value = suggestions[indice].projet_;
-    icon.onclick = () => {
-        lienTag.setAttribute("href", lienPage);
-        lienTag.click();
-    };
-    searchWrapper.classList.remove("active");
-}
-
-function afficheSuggestions(list) {
-    let listData;
-    if (!list.length) {
-        userValue = inputBox.value;
-        listData = `<li>${userValue}</li>`;
-    } else {
-        listData = list.join("");
-    }
-    suggBox.innerHTML = listData;
-}
-
-function togglePopup() {
-    let popup = document.querySelector("#overlay");
-    popup.classList.remove("open");
-}
-
-function detailSaison(Class, balise, indice) {
-    const blockprojet_s = document.querySelector(Class);
-    const listeprojet_ = blockprojet_s.querySelectorAll(balise);
-
-    const destination = "ouvert";
-
-    listeprojet_.forEach(element => {
-        if (element.classList.contains(destination)) {
-            element.classList.remove(destination);
-        }
-    });
-
-    let compteur = 0;
-    listeprojet_.forEach(element => {
-        if (indice == compteur) {
-            element.classList.add(destination);
-        }
-        compteur++;
-    });
-}
-
-function menu_click() {
-    const mnu = document.querySelectorAll('figcaption');
+menu_click.addEventListener('click',() => {
+    document.querySelector('aside').classList.toggle('aside_open');
+    const mnu = document.querySelectorAll('aside span + span');
     mnu.forEach(element => {
-        element.classList.toggle('menu_ouvert');
+        element.classList.toggle('span_visible');
+        console.log(element);
     });
+
+    const titl = document.querySelectorAll('aside h2');
+    titl.forEach(element => {
+        element.classList.toggle('span_visible');
+        console.log(element);
+    });
+    
+    menu_click.classList.toggle('fa-bars');
+    menu_click.classList.toggle('fa-angle-double-left');
+});
+
+/* Sélection du format CV */
+
+const downloadCV = document.querySelector('form');
+downloadCV.addEventListener("submit", (e) => {
+    const data = new FormData(downloadCV);
+    // Prend le format entrer dans le form
+    for (const entry of data) {
+        var lien = document.querySelector('form a');
+        // sélectionne le format à télécharger
+        switch (entry[1]) {
+            case "PDF":
+                lien.setAttribute("href","images/cv/CV_kervadecMatteo.pdf");
+                lien.setAttribute("download","CV_kervadecMatteo.pdf");
+                console.log(lien);
+            break;
+            case "PNG":
+                lien.setAttribute("href","images/cv/CV_kervadecMatteo.png");
+                lien.setAttribute("download","CV_kervadecMatteo.png");
+                console.log(lien);
+            break;
+            case "JPG":
+                lien.setAttribute("href","images/cv/CV_kervadecMatteo.jpg");
+                lien.setAttribute("download","CV_kervadecMatteo.jpg");
+                console.log(lien);
+            break;
+
+            default:
+                alert("format inconnu");
+                break;
+        }
+        lien.click();
+    }
+    e.preventDefault();
+},false);
+
+/* ### Changement de couleur ### */
+
+let colorSelect = 1;
+
+function changColor(color) {
+    if (colorSelect != color) {
+        let sphereColorActual = document.querySelector('.select-color li:nth-child('+color+') span div');
+        const sphereColorOlder = document.querySelector('.select-color li:nth-child('+colorSelect+') span div');
+        if (sphereColorActual == null || sphereColorOlder == null) {
+            alert('couleur inconnu !')
+        } else {
+            sphereColorActual.classList.toggle("selected");
+            sphereColorOlder.classList.toggle("selected");   
+            document.querySelector('.select-color li:nth-child('+color+') span + span').classList.toggle("selected_span");
+            document.querySelector('.select-color li:nth-child('+colorSelect+') span + span').classList.toggle("selected_span");
+            colorSelect = color;
+
+            var root = document.querySelector(':root'); 
+            switch (color) {
+                case 1:
+                    root.style.setProperty("--clr-header-bg--","#364968");
+                    root.style.setProperty("--clr-logo-drpShdw--","#000");
+                    root.style.setProperty("--clr-aside-bg--","#555");
+                    root.style.setProperty("--clr-aside-border--","#ddd");
+                    root.style.setProperty("--clr-aside-selcted--","#ff0000");
+                    root.style.setProperty("--clr-nav-bg--","#E09664");
+                    root.style.setProperty("--clr-nav-hvr--","#555");
+                    root.style.setProperty("--clr-art1-bg--","#FDDF97");
+                    root.style.setProperty("--clr-art2-bg--","#44BEDE");
+                    root.style.setProperty("--clr-section-bg--","#fff");
+                    root.style.setProperty("--clr-search-bg1--","#fff");
+                    root.style.setProperty("--clr-search-bg2--","#eee");
+                    root.style.setProperty("--clr-search-selcted--","#DDD");
+                    root.style.setProperty("--clr-search-border--","#04B3FD");
+                    root.style.setProperty("--clr-search-icon--","#4CA522");
+                    root.style.setProperty("--clr-search-iconhvr--","#0F410E");
+                    root.style.setProperty("--clr-light-font--","#fff");
+                    root.style.setProperty("--clr-dark-font--","#000");
+                    root.style.setProperty("--clr-link-font--","#5cbfca");
+                    root.style.setProperty("--clr-link-hover--","#5cbfca");
+                    root.style.setProperty("--clr-footer-bg--","#6C4343");
+                    break;
+
+                case 2:
+                    console.log(document.querySelector("aside"))
+                    root.style.setProperty("--clr-header-bg--","#3E563D");
+                    root.style.setProperty("--clr-logo-drpShdw--","#000");
+                    root.style.setProperty("--clr-aside-bg--","#314E30");
+                    root.style.setProperty("--clr-aside-border--","#4E923A");
+                    root.style.setProperty("--clr-aside-selcted--","#13558C");
+                    root.style.setProperty("--clr-nav-bg--","#5F635D");
+                    root.style.setProperty("--clr-nav-hvr--","purple");
+                    root.style.setProperty("--clr-art1-bg--","#9BB1BD");
+                    root.style.setProperty("--clr-art2-bg--","#75B6D9");
+                    root.style.setProperty("--clr-section-bg--","#fff");
+                    root.style.setProperty("--clr-search-bg1--","#fff");
+                    root.style.setProperty("--clr-search-bg2--","#eee   ");
+                    root.style.setProperty("--clr-search-selcted--","#75b092");
+                    root.style.setProperty("--clr-search-border--","purple");
+                    root.style.setProperty("--clr-search-icon--","purple");
+                    root.style.setProperty("--clr-search-iconhvr--","purple");
+                    root.style.setProperty("--clr-light-font--","#fff");
+                    root.style.setProperty("--clr-dark-font--","#000");
+                    root.style.setProperty("--clr-link-font--","#39C8DC");
+                    root.style.setProperty("--clr-footer-bg--","#3E565D");
+                    break;
+
+                case 3:
+                    root.style.setProperty("--clr-header-bg--","purple");
+                    root.style.setProperty("--clr-logo-drpShdw--","purple");
+                    root.style.setProperty("--clr-aside-bg--","purple");
+                    root.style.setProperty("--clr-aside-border--","purple");
+                    root.style.setProperty("--clr-aside-selcted--","purple");
+                    root.style.setProperty("--clr-nav-bg--","purple");
+                    root.style.setProperty("--clr-nav-hvr--","purple");
+                    root.style.setProperty("--clr-art1-bg--","purple");
+                    root.style.setProperty("--clr-art2-bg--","purple");
+                    root.style.setProperty("--clr-section-bg--","purple");
+                    root.style.setProperty("--clr-search-bg1--","purple");
+                    root.style.setProperty("--clr-search-bg2--","purple");
+                    root.style.setProperty("--clr-search-selcted--","purple");
+                    root.style.setProperty("--clr-search-border--","purple");
+                    root.style.setProperty("--clr-search-icon--","purple");
+                    root.style.setProperty("--clr-search-iconhvr--","purple");
+                    root.style.setProperty("--clr-light-font--","purple");
+                    root.style.setProperty("--clr-dark-font--","purple");
+                    root.style.setProperty("--clr-link-font--","purple");
+                    root.style.setProperty("--clr-footer-bg--","purple");
+                    break;
+
+                case 4:
+                    root.style.setProperty("--clr-header-bg--","purple");
+                    root.style.setProperty("--clr-logo-drpShdw--","purple");
+                    root.style.setProperty("--clr-aside-bg--","purple");
+                    root.style.setProperty("--clr-aside-border-top--","purple");
+                    root.style.setProperty("--clr-aside-border-bottom--","purple");
+                    root.style.setProperty("--clr-aside-selcted--","purple");
+                    root.style.setProperty("--clr-nav-bg--","purple");
+                    root.style.setProperty("--clr-nav-hvr--","purple");
+                    root.style.setProperty("--clr-art1-bg--","purple");
+                    root.style.setProperty("--clr-art2-bg--","purple");
+                    root.style.setProperty("--clr-section-bg--","purple");
+                    root.style.setProperty("--clr-search-bg1--","purple");
+                    root.style.setProperty("--clr-search-bg2--","purple");
+                    root.style.setProperty("--clr-search-selcted--","purple");
+                    root.style.setProperty("--clr-search-border--","purple");
+                    root.style.setProperty("--clr-search-icon--","purple");
+                    root.style.setProperty("--clr-search-iconhvr--","purple");
+                    root.style.setProperty("--clr-light-font--","purple");
+                    root.style.setProperty("--clr-dark-font--","purple");
+                    root.style.setProperty("--clr-link-font--","purple");
+                    root.style.setProperty("--clr-footer-bg--","purple");
+                    break;
+                    break;
+            }
+        }
+    }
 }
