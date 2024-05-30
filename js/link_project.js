754d80c9898e4d928c246be94f6937772c747602
Matteo-K <kervadecmatteo@gmail.com>
2024-05-30 23:37:38 +0200

    test 3 :3

--- js/link_project.js
+++ js/link_project.js
@@ -4,7 +4,7 @@ let contributeurs;
 
 suggestions.forEach(element => {
     if (/\.html/.test(element.lien)) {
-        fetch("https://Matteo-K/Matteo-K_Portfolio.github.io json/" + element.projet_ + ".json")
+        fetch("/json/" + element.projet_ + ".json")
         .then(response => {
             if (response.ok) {
                 response.json().then(file => {
