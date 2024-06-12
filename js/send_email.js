a673eab8a454dab711ab82a26f874c126507c1b1
Matteo-K <kervadecmatteo@gmail.com>
2024-06-12 16:27:28 +0200

    add diseable button

--- js/send_email.js
+++ js/send_email.js
@@ -10,9 +10,35 @@ function sendMail () {
         subject : document.getElementById("object").value,
         message : document.getElementById("contenue").value + "\nTéléphone : " + document.getElementById("telephone").value
     }
+
     let retour =  {
         email : document.getElementById("email").value
     }
 
     emailjs.send("service_rm1nq1b","template_9bbynqn",params).then(alert("Email send !!!"));
 }
+
+function checkDiseable() {
+    if (input_nom.value.trim() === '' || input_email.value.trim() === '' || input_object.value.trim() === '') {
+        input_submit.disabled = true;
+    } else {
+        input_submit.disabled = false;
+    }
+}
+
+const input_submit = document.getElementById("ft_envoyer");
+const input_nom = document.getElementById("nom");
+const input_email = document.getElementById("email");
+const input_object = document.getElementById("object");
+const form = document.getElementById("form_contact");
+
+input_nom.addEventListener('input', checkDiseable);
+input_email.addEventListener('input', checkDiseable);
+input_object.addEventListener('input', checkDiseable);
+
+
+form.addEventListener('submit', (e) => {
+    if (input_submit.disabled) {
+        e.preventDefault();
+    }
+});
